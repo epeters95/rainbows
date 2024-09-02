ddcb3b289093c28d5c80d1ba23326cbc98cdeaea
Erik <ewpeters@asu.edu>
2024-09-01 17:04:34 -0700

    exploring new configurations

--- droplet.js
+++ droplet.js
@@ -148,9 +148,12 @@
     let y = this.pos[1];
     let shiftScale = this.shiftSlider.getRatio()
     let deltas = [
-      (t) => Math.cos((t + shift) * Math.PI  / 200) * maxIterations * (10 - 20 * shiftScale),
-      (t) => Math.sin((t + shift) * Math.PI / 200) * maxIterations * (10 + 10 * shiftScale),
-      (t) => -Math.cos((t + shift) * Math.PI / 200) * maxIterations * (10 + 20 * shiftScale)
+      // (t) => Math.cos((t + shift) * Math.PI  / 200) * maxIterations * (10 - 20 * shiftScale),
+      // (t) => Math.sin((t + shift) * Math.PI / 200) * maxIterations * (10 + 10 * shiftScale),
+      // (t) => -Math.cos((t + shift) * Math.PI / 200) * maxIterations * (10 + 20 * shiftScale)
+      (t) => Math.cos((t + shift) * Math.PI  / 200) * maxIterations * (10 - 20 * Math.sin(shiftScale) / Math.log(t)),
+      (t) => Math.sin((t + shift) * Math.PI / 200) * maxIterations * (10 - 10 * Math.cos(shiftScale) / Math.log(t)),
+      (t) => -Math.cos((t + shift) * Math.PI / 200) * maxIterations * (10 - 20 * shiftScale)
       // (t) => Math.cos((t + shift)  / 10) * maxIterations * (10 - 20 * shiftScale),
       // (t) => Math.sin((t + shift) / 10) * maxIterations * (10 - 10 * shiftScale),
       // (t) => -Math.cos((t + shift) / 10) * maxIterations * (10 - 20 * shiftScale)
